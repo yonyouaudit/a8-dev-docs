23e66e54260f53ab9e7c54601ab6542a908420ff
sunny <tufpsj@yonyou.com>
2023-07-12 17:11:06 +0800

    # 持续更新文档

--- config/theme.config.jsx
+++ config/theme.config.jsx
@@ -1,5 +1,54 @@
 export default {
-  logo: <span>A8数智化审计平台开发文档</span>,
+  logo: (
+    <>
+      <svg
+        version="1.1"
+        id="Layer_1"
+        x="0px"
+        y="0px"
+        width="60px"
+        height="30px"
+        viewBox="0 0 60 30"
+        enable-background="new 0 0 60 30"
+      >
+        <image
+          id="image0"
+          width="60"
+          height="30"
+          x="0"
+          y="0"
+          href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHEAAABACAMAAAAahmjqAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAB4FBMVEX////nECHlABLtSlf5
+xMjsSFXrP03wbHb//v7pJTT6z9LtTlr97/DygInyg4zrOUf++vr+9/j83+H4ub7pKzr+8/TpJzbm
+Dh/yfofrPUv71tnxcnzwb3nmDB3mDR797O3xdH7qLz7uVWH3sLbtT1v2q7HqLj33rrT3r7XqNELt
+Ul7vX2rnDyDtUV35xcnuVGDpKjnrN0X85ObxdX/vYGvwcHr6ys773N772t372Nv60dTtTFn1maDm
+CxzsQE3//f3mBhjmChvxc3384OL4vMHzipLyf4jnFyf0lp3zho/waXT1oKfrO0nlAxXrOEbqLTzz
+iZHpKTjnESLvYWz++PjoITH0kJj2o6n0lJznFibnEyT+8fLwa3X5x8vnEiP84eP6zND0kZnqNUP+
+9vf98PH++fnoIjLsRVL1n6boHS3oGir0j5frPkz2qrDuW2f/+/vsRlPlBBb3s7jxeYLxdoDvZnHz
+i5P4u8D+8vPpJjXtUFzmBRf+9fb719rqMD/oGyvrPEr4t7zoHCzzh4/nFSb7297rNkTqMkD4vcL7
+2dzqMUDlARPtU1/uWmbyhI3zjJToHi73rbPuVmL5wcX96uv1naTsSVbpIzP//Pz85uj6y8/0mJ/o
+Hy/0kpr2pqz4v8SoapkiAAAAAWJLR0QAiAUdSAAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAAd0SU1F
+B+QJGAsEBLboAMkAAAL4SURBVFjD7ZfpW1JBFMZHKkAzAkwxIQyjVDANs6CMKFvICgvDDCmsSCtb
+yBbbzKUyStttof1fjTtn7gpc5tL1earnvp/mPXPm/O4ynDsgpEmTJk2a/mdV6LCWyeUsX6HPyWBU
+h1gJRF2VTM5KSKlWh7iKEE0yOashxVwWwGKVqIYQ10gnavlFdTjDVt4t1etotRahBrsDax0OOB3F
+ZG+UIa6nJroQaqLN3aAW0U2bu/EvJG5qbpFRK0v00BKbSxK9goinjdFmQcTLEts73Iy2QFmfu5g6
+OmWIW/HiLkHExdbn1IUjrZzfBsTt5f06/Pi5BWSJO3DOTs53A7GRGiLVLr1IQVwuKIrtFm0FaADO
+soFUO9AtyA/twaG9BV+hpyegPnGffKrNqjpxf4ncA5RE30GiMLZh1vryiIdKEHspiQ7WSvaqI494
+uATxiOrEozgSyfti9BGiX22i0Ykjx/LqHAdgtCSQEKNeon5s+1kblRIDUPiEtExsACZO0hKp9+og
+RE4hSxh34HgNxE0QH0qoTjwNkTMIGWCUHGbCZ6Ev6M6VBiolnoeNk0LoQhwm+0YQqh6FsctMTbx4
+iegytmNXiK5KiIkIDviY8TVyPdeN6RswGq+iACrcqzeh9C1svAR5+w4ZTNAAFRLB6+5ic++++OE/
+oDumKyPCAeDhJLhHbULgVC0VkBCni3wfp8VEP9SeYf3sOA983EkHVLZXn4Dnu/Ugl/N0jhKojPgM
+fAO3OpVhc8aeLwWRtLg6bvGLeT5poWeSjviSgviK5NrBviY2/SYoSnv7jorY3iQ6qdjw2iFR7D05
+yFmScDO5g0VoeG7RNJB3aZmJResHC+3jBbnEm1Gkj1D2U27YK+Q49CJsRagMoqvQVPozVJxFXCdg
+lGxBWVNEiFR2kzJEcsKZN4qI3V+Yua+ZJSBm4/wtcsRK9r9i9tv3PyJOFXqouIs667H5sZDrdaPu
+n2l+3vjLAH02ruw9WhKMUorWCKjmkVhMGVCTJk2a/nX9Bv2Bka4bC4+2AAAAJXRFWHRkYXRlOmNy
+ZWF0ZQAyMDIwLTA5LTI0VDExOjA0OjA0KzAwOjAwb++b9QAAACV0RVh0ZGF0ZTptb2RpZnkAMjAy
+MC0wOS0yNFQxMTowNDowNCswMDowMB6yI0kAAAAASUVORK5CYII="
+        />
+      </svg>
+      <span>A8数智化审计平台开发文档</span>
+    </>
+  ),
   // navTheme: "dark",
   project: {
     link: "https://git.yonyouaud.com/a8/frontend.git",
@@ -17,10 +66,10 @@ export default {
     component: null,
   },
   toc: {
-    title: <>页内导航</>
+    title: <>页内导航</>,
   },
   editLink: {
-    text: <>编辑此页</>
+    text: <>编辑此页</>,
   },
   feedback: { content: null },
   docsRepositoryBase: "https://git.yonyouaud.com/a8/a8-dev-docs/-/tree/master",
